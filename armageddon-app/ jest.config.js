e4d22d57714cc2234098af70f125e617eed626b3
ekaterinakoshitc <ekaterinakoshitc@gmail.com>
2025-03-21 16:24:24 +0300

    Updates

--- armageddon-app/ jest.config.js
+++
@@ -1,4 +0,0 @@
-export default {
-    moduleDirectories: ["node_modules", "src"],
-    testEnvironment: "jsdom",
-};
--- armageddon-app/src/pages/Asteroid.jsx
+++
@@ -1,8 +0,0 @@
-import {useParams} from "react-router-dom";
-import {Header} from "../components/header/Header";
-
-export const Asteroid = () => {
-    const {id} = useParams()
-    return <div><Header/>{`Asteroid page ${id}`}</div>
-
-}
